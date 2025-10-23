060a0b16d2755dba3c8abee3f1e4663655d067fb
Ariel Aguilar <arielaguilar1994@gmail.com>
2025-10-23 21:58:31 +0200

    wip: add db name like var environment

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,6 +2,7 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   MONGODB: Joi.required(),
+  DB_NAME: Joi.required(),
   PORT: Joi.number().default(3002),
   DEFAULT_LIMIT: Joi.number().default(3)
 });
